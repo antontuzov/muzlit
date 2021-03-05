b8cb1a75443166672c880faefae1746aeaf4553f
AntonTuzov <antontuzovf@gmail.com>
2021-03-05 11:46:00 +0300

    lazy drop

--- muzlit/Manager/APICaller.swift
+++ muzlit/Manager/APICaller.swift
@@ -421,6 +421,97 @@ final class APICaller {
         }
     
     
+    
+    
+    // MARK: - Category
+        public func getCategories(completion: @escaping (Result<[Category], Error>) -> Void) {
+            createRequest(
+                with: URL(string: Constants.baseAPIURL + "/browse/categories?limit=50"),
+                type: .GET
+            ) { request in
+                let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                    guard let data = data, error == nil else{
+                        completion(.failure(APIError.faileedToGetData))
+                        return
+                    }
+
+                    do {
+                        let result = try JSONDecoder().decode(AllCategoriesResponse.self,
+                                                              from: data)
+                        completion(.success(result.categories.items))
+                    }
+                    catch {
+                        completion(.failure(error))
+                    }
+                }
+                task.resume()
+            }
+        }
+
+        public func getCategoryPlaylists(category: Category, completion: @escaping (Result<[Playlist], Error>) -> Void) {
+            createRequest(
+                with: URL(string: Constants.baseAPIURL + "/browse/categories/\(category.id)/playlists?limit=50"),
+                type: .GET
+            ) { request in
+                let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                    guard let data = data, error == nil else{
+                        completion(.failure(APIError.faileedToGetData))
+                        return
+                    }
+
+                    do {
+                        let result = try JSONDecoder().decode(CategoryPlaylistsResponse.self, from: data)
+                        let playlists = result.playlists.items
+                        completion(.success(playlists))
+                    }
+                    catch {
+                        completion(.failure(error))
+                    }
+                }
+                task.resume()
+            }
+        }
+
+        // MARK: - Search
+        public func search(with query: String, completion: @escaping (Result<[SearchResult], Error>) -> Void) {
+            createRequest(
+                with: URL(string: Constants.baseAPIURL+"/search?limit=10&type=album,artist,playlist,track&q=\(query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")"),
+                type: .GET
+            ) { request in
+                print(request.url?.absoluteString ?? "none")
+                let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                    guard let data = data, error == nil else {
+                        completion(.failure(APIError.faileedToGetData))
+                        return
+                    }
+
+                    do {
+                        let result = try JSONDecoder().decode(SearchResultsResponse.self, from: data)
+
+                        var searchResults: [SearchResult] = []
+                        searchResults.append(contentsOf: result.tracks.items.compactMap({ .track(model: $0) }))
+                        searchResults.append(contentsOf: result.albums.items.compactMap({ .album(model: $0) }))
+                        searchResults.append(contentsOf: result.artists.items.compactMap({ .artist(model: $0) }))
+                        searchResults.append(contentsOf: result.playlists.items.compactMap({ .playlist(model: $0) }))
+
+                        completion(.success(searchResults))
+                    }
+                    catch {
+                        completion(.failure(error))
+                    }
+                }
+                task.resume()
+            }
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 //    public func addTrackToPlaylist(
 //            track: AudioTrack,
 //            playlist: Playlist,
